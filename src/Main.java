487aa2651f20349acd339c4d7ec54bb381d75ff4
Tazabay Kassym <tazabaykassym@gmail.com>
2026-09-09 13:08:31 +0500

    Create first Java practice

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Java!");
+        String product = "Coffee";
+        int quantity = 3;
+        double price = 1250.0;
+        double discountPercent = 10.0;
+        double a = quantity * price;
+        double b = a * (discountPercent / 100);
+        double c = a - b;
+        System.out.println("Товар: " + product);
+        System.out.println("Количество: " + quantity);
+        System.out.println("Стоимость без скидки: " + a);
+        System.out.println("Сумма скидки: " + b);
+        System.out.println("Итоговая стоимость: " + c);
     }
 }
